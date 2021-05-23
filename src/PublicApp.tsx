60e6ef69dace7bcbc7e1fddcd80d1f5a460b68e5
trevv16 <tnjeru16@gmail.com>
2021-05-23 15:27:47 -0400

    changed router to map array of routes

--- src/PublicApp.tsx
+++ src/PublicApp.tsx
@@ -1,8 +1,55 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { HomePage, Error500, Error404, SignUpPage, SignInPage, ForgotPage, ResetPasswordPage } from './views/index';
+import { HomePage, SignUpPage, SignInPage, ForgotPage, ResetPasswordPage, Error500, Error404 } from './views/index';
 import { Navigation, Footer, PublicLayout } from './components/index';
+
+const SITE_NAME = 'Caption Search';
+const SITE_IMG = '';
+const SITE_IMG_ALT = '';
+
+const pages = [
+  {
+    route: '/',
+    title: `${SITE_NAME}`,
+    description: '',
+    image: SITE_IMG,
+    image_alt: SITE_IMG_ALT,
+    component: HomePage
+  },
+  {
+    route: '/signup',
+    title: `Sign Up | ${SITE_NAME}`,
+    description: '',
+    image: SITE_IMG,
+    image_alt: SITE_IMG_ALT,
+    component: SignUpPage
+  },
+  {
+    route: '/signin',
+    title: `Sign In | ${SITE_NAME}`,
+    description: '',
+    image: SITE_IMG,
+    image_alt: SITE_IMG_ALT,
+    component: SignInPage
+  },
+  {
+    route: '/forgot',
+    title: `Forgot Password | ${SITE_NAME}`,
+    description: '',
+    image: SITE_IMG,
+    image_alt: SITE_IMG_ALT,
+    component: ForgotPage
+  },
+  {
+    route: '/reset-password/:resetToken',
+    title: `Reset Password | ${SITE_NAME}`,
+    description: '',
+    image: SITE_IMG,
+    image_alt: SITE_IMG_ALT,
+    component: ResetPasswordPage
+  }
+];
 
 export default function PublicApp() {
   return (
@@ -12,12 +59,9 @@ export default function PublicApp() {
         <PublicLayout>
           <Switch>
             {/* Auth Routes */}
-            <Route exact path='/signup' component={SignUpPage} />
-            <Route exact path='/signin' component={SignInPage} />
-            <Route exact path='/forgot' component={ForgotPage} />
-            <Route exact path='/reset-password/:resetToken' component={ResetPasswordPage} />
-
-            <Route exact path='/' component={HomePage} />
+            {pages.map((page: PageProps) => {
+              return <Route exact path={page.route} component={page.component} props={page} />;
+            })}
 
             {/* Error Pages */}
             <Route exact path='/500' component={Error500} />
