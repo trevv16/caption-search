d04a3b667d64499d92405d98a20595e9bf0d7389
trevv16 <tnjeru16@gmail.com>
2021-05-23 15:26:06 -0400

    added typescript types

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { SeoHelmet } from '../components/index';
 
-export default function HomePage() {
+const HomePage: FC<PageProps> = ({ title, description, image, image_alt }) => {
   return (
     <>
-      <SeoHelmet title='' description='' image='' image_alt='' />
+      <SeoHelmet title={title} description={description} image={image} image_alt={image_alt} />
       <div></div>
     </>
   );
-}
+};
+
+export default HomePage;
