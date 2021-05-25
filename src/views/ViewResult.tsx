3118605707aa6da2571e064622c1b2479555481a
trevv16 <tnjeru16@gmail.com>
2021-05-24 20:44:37 -0400

    properly renders video result from url in params

--- src/views/ViewResult.tsx
+++ src/views/ViewResult.tsx
@@ -1,27 +1,30 @@
 import React, { useState, useEffect, FC } from 'react';
 import { SeoHelmet } from '../components/index';
 import ReactPlayer from 'react-player/youtube';
+import { useLocation } from 'react-router-dom';
+import { stripUrlParam } from '../utils/youtube';
 
-type CustomPageProps = {
-  path: string;
-  title: string;
-  description: string;
-  image: string;
-  image_alt: string;
-  component: any;
-  location: any;
-};
+/**
+ * https://www.youtube.com/watch?v=yHdLoVpzdcg
+ * https://www.youtube.com/watch?v=SXKPBh12WbE&list=PLhKmHfWT5YmKq8M3lrpGBaUDRToxnUAJF&index=1
+ *
+ * https://www.youtube.com/playlist?list=PLhKmHfWT5YmKq8M3lrpGBaUDRToxnUAJF
+ */
 
-const ViewResult: FC<CustomPageProps> = ({ title, description, image, image_alt, location }) => {
-  const [searchUrl, setSearchUrl] = useState(location.state?.url);
+const ViewResult: FC<PageProps> = ({ title, description, image, image_alt }) => {
+  const location = useLocation();
+  const urlParam = location.search;
+  const [searchUrl, setSearchUrl] = useState('');
 
   useEffect(() => {
-    if (location.state?.url) {
-      setSearchUrl(location.state.url);
-    } else {
-      setSearchUrl('');
+    if (urlParam === undefined) return;
+
+    const url = stripUrlParam(urlParam, 'url');
+
+    if (url) {
+      setSearchUrl(url);
     }
-  }, [location.state?.url]);
+  }, [urlParam]);
 
   return (
     <>
