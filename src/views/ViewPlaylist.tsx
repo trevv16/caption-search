fc00f9e5a349ac588d8536644cd8ca50bb311eb0
trevv16 <tnjeru16@gmail.com>
2021-05-24 20:48:02 -0400

    start of playlist functionality

--- src/views/ViewPlaylist.tsx
+++ src/views/ViewPlaylist.tsx
@@ -1,7 +1,35 @@
-import React, { FC } from 'react';
+import React, { useState, useEffect, FC } from 'react';
 import { SeoHelmet } from '../components/index';
+import ReactPlayer from 'react-player/youtube';
+import { useLocation } from 'react-router-dom';
+import { stripUrlParam } from '../utils/youtube';
+import ytfps from 'ytfps';
 
 const ViewPlaylist: FC<PageProps> = ({ title, description, image, image_alt }) => {
+  const location = useLocation();
+  const urlParam = location.search;
+  const [playlist, setPlaylist] = useState<any>(null);
+
+  useEffect(() => {
+    if (urlParam === undefined) return;
+
+    const tempUrl = stripUrlParam(urlParam, 'url');
+    const youtubeUrl = tempUrl.split('?')[1];
+    const searchUrl = stripUrlParam(youtubeUrl, 'list');
+
+    if (searchUrl === undefined) return;
+
+    ytfps(searchUrl)
+      .then((playlist) => {
+        setPlaylist(playlist);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }, [urlParam]);
+
+  console.log(playlist);
+
   return (
     <>
       <SeoHelmet title={title} description={description} image={image} image_alt={image_alt} />
