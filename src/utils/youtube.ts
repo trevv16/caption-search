65ecc4c104e69fed366bdde0487354519332d239
trevv16 <tnjeru16@gmail.com>
2021-05-25 21:26:20 -0400

    fetch to lambda function to get playlist data

--- src/utils/youtube.ts
+++ src/utils/youtube.ts
@@ -1,7 +1,30 @@
+import api from './api';
+import { LAMBDA_URL } from '../Config';
+const ytch = require('yt-channel-info');
 const queryString = require('query-string');
 
 const isPlaylist = (url: string): boolean => {
   return url.includes('/playlist');
+};
+
+const getChannelInfo = (channelId: string) => {
+  // const data: any = await api.fetch(`${LAMBDA_URL}/channelInfo?channelId=${channelId}`);
+
+  // return data;
+  ytch
+    .getChannelInfo(channelId)
+    .then((response: any) => {
+      return response;
+    })
+    .catch((err: any) => {
+      console.log(err);
+    });
+};
+
+const getPlaylistData = async (playlistId: string) => {
+  const data: any = await api.fetch(`${LAMBDA_URL}/playlistVideos?playlistId=${playlistId}`);
+
+  return data;
 };
 
 const stripUrlParam = (searchUrl: string, query: string): string => {
@@ -10,4 +33,4 @@ const stripUrlParam = (searchUrl: string, query: string): string => {
   return parsed[query];
 };
 
-export { isPlaylist, stripUrlParam };
+export { isPlaylist, stripUrlParam, getPlaylistData, getChannelInfo };
