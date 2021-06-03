2ed4c6148ad2b3c85e8d7b3b40fb3ce2ccb51bea
trevv16 <tnjeru16@gmail.com>
2021-06-02 20:02:10 -0400

    fixed playlist grid error

--- src/components/YoutubePlaylist.tsx
+++ src/components/YoutubePlaylist.tsx
@@ -23,7 +23,7 @@ export default function YoutubePlaylist(props: any) {
 
   return (
     <>
-      <div className='grid grid-cols-3 gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+      <div className='grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3'>
         {props.data &&
           props.data.map((video: any) => {
             return (
